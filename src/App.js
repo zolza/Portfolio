73571d656bce15d53c1976b604cb6ce8f292ed35
Kinga Kozioł <kingster@kingas-air.home>
2017-06-03 15:00:13 +0100

    Add hero section, font, basic styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Hero from './hero/Hero'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Hero />
         <div className="App-header">
         </div>
       </div>
---
+++ src/hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <div className="Hero_header_section">
+        <h1 className="Hero_header">Kinga Koziol</h1>
+        <p className="Hero_subheader">Front-end developer</p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
