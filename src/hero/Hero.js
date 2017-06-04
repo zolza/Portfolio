8b5e4d32b2c00656a4958ef6656285a2cadc16f2
Kinga Kozioł <zolza@users.noreply.github.com>
2017-06-04 22:33:12 +0100

    Change hero image, add more styling.

--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -5,8 +5,10 @@ const Hero = () => {
   return (
     <div className="Hero">
       <div className="Hero_header_section">
-        <h1 className="Hero_header">Kinga Koziol</h1>
-        <p className="Hero_subheader">Front-end developer</p>
+        <h1 className="Hero_header">Hello there!</h1>
+        <p className="Hero_subheader">My name is Kinga</p>
+        <p className="Hero_subheader">I'm a front-end developer</p>
+
       </div>
     </div>
   );
